18d884e649367470dd415f1dacfb126f0ac32ba2
Mario Hernandez <gansel1093@gmail.com>
2017-09-11 11:06:58 -0600

    Intercambio inicial de proyecto

---
+++ Pregunta.java
@@ -0,0 +1,24 @@
+
+/**
+ * Cuestionario
+ * 
+ * @author (El niño) 
+ * @version (1.0)
+ */
+public class Pregunta
+{
+    
+    private String texto;
+    private String respuesta;
+
+    /**
+     * Constructor for objects of class Pregunta
+     */
+    public Pregunta(String unTexto, String respuesta)
+    {
+        this.respuesta = respuesta;
+        texto = unTexto;
+    }
+
+   
+}
